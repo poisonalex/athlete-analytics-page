c43be2f411531cc17cb7cf2d904eec6a242432a3
poisonalex <131189491+poisonalex@users.noreply.github.com>
2025-02-04 12:40:45 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Willkommen auf meiner GitHub Pages Website!</h1>
+    <p>Dies ist eine einfache Vue.js-Seite, die mit Vite erstellt wurde.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
